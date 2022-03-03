c8b4e58903f610c5215e3afb1d4f79308ff7bfe6
Duramann <theo.dura@efrei.net>
2022-03-03 22:47:33 +0100

    HTML, CSS and first flask app version

---
+++ web_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+from detoxify import Detoxify
+import nltk
+from nltk.corpus import stopwords
+from string import punctuation
+
+nltk.download('stopwords')
+stop_words = stopwords.words('english')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == "GET":
+        return redirect('/')
+        
+    if request.method == 'POST':
+        rawtext = request.form.values()
+        texte = [str(x) for x in rawtext]
+        to_process = " ".join([w for w in texte])
+            
+        model = Detoxify('original')
+        result = model.predict(to_process)
+        
+        printtexte = " ".join([w for w in texte])
+        
+
+        return render_template('index.html', result=result, topredict='The sentence to analyse was : "' + printtexte + '"')
